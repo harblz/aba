78b1c72ed1a6a63820703e18ece7ad152994eb2e
Harblz <harblz@gmail.com>
2017-12-11 23:44:54 -0500

    added html5 calendar icon

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from ckeditor.fields import RichTextField
+from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
