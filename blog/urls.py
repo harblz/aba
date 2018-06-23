5fc9eb27fa8a37ce6ab1f95c46999e766a948f5b
Alex <harblz@gmail.com>
2018-06-23 09:31:13 -0400

    fixing disqus comment threads... hopefully

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_details_by_id, name='post_details_by_id'),
+    path('<slug:slug>/', views.post_details_by_slug, name='post_details_by_slug'),
 
     url(r'^research$', views.blog_research, name='research'),
     url(r'^coffee$', views.blog_coffee, name='coffee'),
