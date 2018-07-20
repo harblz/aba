af8a6a0a84b98082885dc0a8893c06b27cc0f623
Alex <harblz@gmail.com>
2018-07-19 20:00:56 -0400

    fixing the stripe problem

--- abarocks/views.py
+++ abarocks/views.py
@@ -1,3 +1,5 @@
+import stripe
+
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -19,3 +21,30 @@ def redirect_coffee(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     pages = Pages.objects.order_by('order')
     return render(request, 'blog/blog_coffee.html', { 'pages': pages })
+
+
+def redirect_coffee_confirm(request):
+    stripe.api_key = "sk_live_0fHEU5T1nHoILQpYZ7lyPwP7"
+
+    if request.method == "POST":
+        token    = request.POST.get("stripeToken")
+        donation = request.POST.get("donation")
+
+    try:
+        charge  = stripe.Charge.create(
+            amount      = donation,
+            currency    = "usd",
+            source      = token,
+            description = "A generous donation from a fan!"
+        )
+
+        # new_car.charge_id   = charge.id
+
+    except stripe.error.CardError as ce:
+        return False, ce
+
+    else:
+        #new_car.save()
+        donation = int(donation) / 100.00
+        return render(request, 'blog/thanks.html', { 'donation' : donation })
+        # The payment was successfully processed & the user's card was charged - confirm with them how much
