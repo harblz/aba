a0b82b88bf76ffec3424fd5a5b3e59af32780887
Alex <harblz@gmail.com>
2018-08-26 16:32:39 -0400

    testing the new 'forms' feature for quizzes; testing will be done in the most manly way possible - deploying straight to production

--- quiz/admin.py
+++ quiz/admin.py
@@ -4,7 +4,7 @@ from django.utils.html import format_html
 
 from django.contrib.admin import SimpleListFilter
 
-from .models import Unit, Difficulty, Choice, Question, Task
+from .models import Unit, Form, Difficulty, Choice, Question, Task
 
 # Extending SimpleListFilter
 
@@ -17,6 +17,20 @@ class ChoiceInline(admin.TabularInline):
 class UnitInline(admin.TabularInline):
     model = Unit
 admin.site.register(Unit)
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ['form_name', 'form_unit_id', 'form_description']
+    list_per_page = 100000
+    list_filter  = ['form_name', 'form_unit_id']
+    search_fields = [  'form_name', 'form_description' ]
+
+    fieldsets = [
+        ( 'Form Info',
+            {'fields': ['form_name', 'form_unit_id']}),
+        ('Describe this form, briefly', {'fields': ['form_description'], 'classes': ['']}), # put "collapse" in classes to auto-hide it; requires bootstrap
+    ]
+admin.site.register(Form, FormAdmin)
 
 
 class TaskInline(admin.TabularInline):
@@ -35,9 +49,9 @@ change_difficulty_RBT.short_description = "Change selected questions difficulty 
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('escaped_question_text', 'question_unit_name', 'has_answer', 'question_difficulty', 'task_list_item', 'unit_id', 'pub_date', 'was_published_recently')
+    list_display = ('escaped_question_text', 'question_unit_name', 'question_form_name', 'has_answer', 'question_difficulty', 'task_list_item', 'pub_date', 'was_published_recently')
     list_per_page = 100000
-    list_filter = ['unit_id', 'task_list_item', 'pub_date', ]
+    list_filter = ['unit_id', 'task_list_item', 'form_id', 'pub_date', ]
 
     # actions = [change_unit]
 
@@ -46,6 +60,9 @@ class QuestionAdmin(admin.ModelAdmin):
 
     def question_unit_name(self, obj):
         return Unit.objects.get(pk=obj.unit_id)
+
+    def question_form_name(self, obj):
+        return Form.objects.get(pk=obj.unit_id)
 
     def has_answer(self, obj):
         choices = Choice.objects.filter(question_id=obj.id)
@@ -64,12 +81,25 @@ class QuestionAdmin(admin.ModelAdmin):
         return Task.objects.get(pk=obj.task_list_item)
 
     escaped_question_text.allow_tags = True
+    
     escaped_question_text.short_description = 'Question text'
+
+    question_form_name.short_description    = 'Form'
+    question_form_name.admin_order_field    = 'form__form_name'
+
+    question_unit_name.short_description    = 'Unit'
+    question_unit_name.admin_order_field    = 'unit__unit_name'
+
+    question_difficulty.short_description   = 'Difficulty'
+
+
+    has_answer.admin_order_field            = 'choice__is_correct'
+    question_difficulty.admin_order_field   = 'difficulty__difficulty_name'
     
     fieldsets = [
         (None,               {'fields': ['question_text', 'task_list_item']}),
         ('Question Hint', {'fields': ['question_hint'], 'classes': ['']}), # put "collapse" in classes to auto-hide it; requires bootstrap
-        ('Unit / Course Information', {'fields': [ 'unit'], 'classes': ['']}),
+        ('Unit / Course Information', {'fields': [ 'unit', 'form' ], 'classes': ['']}),
         ('Difficulty', {'fields': [ 'difficulty'], 'classes': ['']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['']}),
     ]
--- quiz/urls.py
+++ quiz/urls.py
@@ -6,4 +6,6 @@ app_name = 'quiz'
 urlpatterns = [
     url(r'^$', views.quiz_index, name='index'),
     url(r'^(?P<quiz_id>\w+)/$', views.quiz_view, name='quiz'),
+    url(r'^(?P<quiz_id>\w+)/form_(?P<form_id>\w+)/$', views.quiz_view, name='quiz'),
+    url(r'^(?P<quiz_id>\w+)/form_(?P<form_id>\w+)/grade_question', views.quiz_view, name='quiz'),
 ]
