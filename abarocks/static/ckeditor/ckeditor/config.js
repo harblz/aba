d583fb2b3c1982c1c6d514277585fed7855f85cf
Alex <harblz@gmail.com>
2018-11-15 13:58:07 -0500

    fixing carousel (somewhat), adding categories, adding tags

--- abarocks/static/ckeditor/ckeditor/config.js
+++ abarocks/static/ckeditor/ckeditor/config.js
@@ -7,4 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.disableNativeSpellChecker = false;
+	config.scayt_autoStartup = true;
 };
