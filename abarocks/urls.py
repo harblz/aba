111dfa5e8a835d695be42b64dbecb44b2a21a2e4
Alex <harblz@gmail.com>
2019-04-06 18:14:19 -0400

    adding wagtail for testing

--- abarocks/urls.py
+++ abarocks/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import include, url
-from django.urls import path
+from django.urls import path, re_path, include
 from django.contrib import admin
 
 from django.views.generic import RedirectView
+
+# wagtail CMS
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
 
 from .views import redirect_error_report, redirect_root, redirect_study, redirect_research, redirect_coffee, redirect_coffee_confirm
 
@@ -34,7 +42,12 @@ urlpatterns = [
     url(r'^quiz/', include('quiz.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^fluency/', include('fluency.urls')),
-]
+
+    #wagtail CMS
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^wag_pages/', include(wagtail_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_title  = 'ABA.rocks Administration'
 admin.site.site_header = 'ABA.rocks administration'
