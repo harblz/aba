99dec7be1d9ba2dbc5a03e735eeaebcb80653015
Kyle Jensen <20227912+PhoenixStorm1015@users.noreply.github.com>
2024-09-22 14:43:11 -0400

    Added Werkzeug

--- pages/models.py
+++ pages/models.py
@@ -3,27 +3,27 @@ from django.utils import timezone
 from ckeditor.fields import RichTextField
 from datetime import datetime
 
+
 # Create your models here.
 class Pages(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
-    title  = models.CharField(max_length=200)
-    slug   = models.CharField(max_length=200)
-    text   = RichTextField()
-    order  = models.IntegerField()
-    icon   = models.CharField(max_length=50)
-    STATUS_CHOICES = (
-        ('d', 'Draft'),
-        ('p', 'Published'),
-        ('w', 'Withdrawn'),
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
     )
-
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    text = RichTextField()
+    order = models.IntegerField()
+    icon = models.CharField(max_length=50)
+    STATUS_CHOICES = (
+        ("d", "Draft"),
+        ("p", "Published"),
+        ("w", "Withdrawn"),
+    )
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
     page_views = models.IntegerField(default=0)
-
 
     class Meta:
         verbose_name_plural = "Site Pages"
