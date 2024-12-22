ef00043e4aecaf9af23d0de80412018c2bc2fc8b
Alexander Raymond-Jones <harblz@gmail.com>
2024-12-22 18:54:24 -0500

    less angry pycharm

--- blog/static/js/google-analytics.js
+++ blog/static/js/google-analytics.js
@@ -1,10 +1,18 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+// Google tag(gtag.js)
+function loadGoogleAnalytics() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-113XQ18ZQ6';
 
-  ga('create', 'UA-98470698-1', 'auto');
-  ga('send', 'pageview');
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+}
 
-</script>
+loadGoogleAnalytics(); //Create the script  
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-113XQ18ZQ6');
