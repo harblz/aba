f0826819ec681ca723a4ff541cd180a02b9c673e
Kyle Jensen <kyle@nullandvoid.digital>
2024-12-29 10:40:57 -0500

    lots of javascript config

---
+++ webpack.prod.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./staticfiles/js/main.js",
+  output: {
+    filename: "aba.js",
+    path: path.resolve(__dirname, "./staticfiles/js"),
+  },
+};
