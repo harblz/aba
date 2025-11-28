40b5c886fb4a6940a695eace8342e1ba3a8c2276
Kyle Jensen <kyle@nullandvoid.digital>
2025-11-27 20:31:54 -0500

    Update `gunicorn.conf.py`: modify `accesslog` path to use a relative directory.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "0.0.0.0:8000"
-accesslog = "/var/log/gunicorn/access.log"
+accesslog = "./access.log"
 def ssl_context(conf, default_ssl_context_factory):
     import ssl
     context = default_ssl_context_factory()
